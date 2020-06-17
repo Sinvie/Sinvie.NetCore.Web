4f9142ace89b603e866aa34411129d7ea3d7ca3a
zouxingwu <qq@qq.com>
2020-06-17 09:14:06 +0800

    新增多个文件

--- Sinvie.Cms.WebUI/Controllers/ContentController.cs
+++ Sinvie.Cms.WebUI/Controllers/ContentController.cs
@@ -16,7 +16,7 @@ namespace Sinvie.Cms.WebUI.Controllers
             var contents = new List<M_T_Content>();
             for (int i = 1; i < 11; i++)
             {
-                contents.Add(new M_T_Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = i % 2 == 0 ? 1 : 0, add_time = DateTime.Now.AddDays(-i) });
+                contents.Add(new M_T_Content { Id = i+1, title = $"{i}的标题", content = $"{i}的内容", status = i % 2 == 0 ? 1 : 0, add_time = DateTime.Now.AddDays(-i) });
             }
             return View(new M_T_ViewContent { Contents = contents });
         }
